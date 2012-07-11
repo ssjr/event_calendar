a5de3ec7151cfed811b86c8597ac0d2aaa483fc6
Sérgio Schnorr Júnior <sergio.schnorr@caixadeideias.com.br>
2012-07-11 15:45:53 -0300

    occupied

--- lib/event_calendar/calendar_helper.rb
+++ lib/event_calendar/calendar_helper.rb
@@ -183,7 +183,7 @@ module EventCalendar
           cal << %(ec-today-header ) if options[:show_today] and (day == Date.today)
           cal << %(ec-other-month-header ) if (day < first) || (day > last)
           cal << %(ec-weekend-day-header ) if weekend?(day)
-          cal << %(occuped)
+          cal << %(occupied)
           cal << %(" style="height: #{options[:day_nums_height]}px;">)
           if options[:link_to_day_action]
             cal << day_link(day.day, day, options[:link_to_day_action])
